0cce1ce2546453448207637f90ec2e7c0337e96a
DTDASchool <51952304+DTDASchool@users.noreply.github.com>
2019-06-18 12:12:20 +0200

    Add files via upload

---
+++ Assets/script/mouvement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouvement : MonoBehaviour
+{
+	public float mouvSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float tempV = Input.GetAxis ("Vertical");
+		float tempH = Input.GetAxis ("Horizontal");
+		Debug.Log(transform.position);
+		
+		transform.position = new Vector3 (transform.position.x + tempH * Time.deltaTime * mouvSpeed, transform.position.y, transform.position.z + tempV * Time.deltaTime * mouvSpeed);
+        
+
+    }
+}
